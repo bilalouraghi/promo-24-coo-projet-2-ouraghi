6ad907f000d2c234ace8144659b2b5c023c87807
Bilal ouraghi <ouraghib@3il.fr>
2026-04-01 10:12:46 +0200

    feat: implémentation de la classe StandardPress.java

--- src/duckcorp/machine/StandardPress.java
+++ src/duckcorp/machine/StandardPress.java
@@ -1,19 +1,59 @@
 package duckcorp.machine;
 
 import duckcorp.duck.Duck;
+import duckcorp.duck.DuckType;
+import duckcorp.duck.StandardDuck;
 
 /**
  * Presse produisant des canards Standard.
  *
  * TODO (Ex2) :
- *   - Faites hériter cette classe de Machine
- *   - Implémentez le constructeur sans paramètre avec un appel à super
- *   - Implémentez produceDuck(), getPurchaseCost(), getName()
+ * - Faites hériter cette classe de Machine
+ * - Implémentez le constructeur sans paramètre avec un appel à super
+ * - Implémentez produceDuck(), getPurchaseCost(), getName()
+ * 
  * @author Roussille Philippe <roussille@3il.fr>
  */
-public class StandardPress {
+public class StandardPress extends Machine {
 
-    public static final int PURCHASE_COST    = 500;
-    public static final int CAPACITY         = 5;
+    public static final int PURCHASE_COST = 500;
+    public static final int CAPACITY = 5;
     public static final int MAINTENANCE_COST = 50;
+
+    /**
+     * Crée une nouvelle presse standard.
+     */
+    public StandardPress() {
+        super(DuckType.STANDARD, CAPACITY, MAINTENANCE_COST);
+    }
+
+    /**
+     * Produit un canard standard avec une qualité dépendante de l'état de la presse.
+     *
+     * @return un nouveau canard standard
+     */
+    @Override
+    public Duck produceDuck() {
+        return new StandardDuck(computeQuality());
+    }
+
+    /**
+     * Retourne le coût d'achat d'une presse standard.
+     *
+     * @return le coût : 500 euros
+     */
+    @Override
+    public int getPurchaseCost() {
+        return PURCHASE_COST;
+    }
+
+    /**
+     * Retourne le nom commercial de cette presse.
+     *
+     * @return "Presse Standard"
+     */
+    @Override
+    public String getName() {
+        return "Presse Standard";
+    }
 }
