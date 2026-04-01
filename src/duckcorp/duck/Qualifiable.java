148cbdaffb2f9b304e9636c9a9d6f764735f0582
Bilal ouraghi <ouraghib@3il.fr>
2026-04-01 09:19:01 +0200

    feat : ajout documentation + méthode getQualityLabel()

--- src/duckcorp/duck/Qualifiable.java
+++ src/duckcorp/duck/Qualifiable.java
@@ -10,13 +10,21 @@ package duckcorp.duck;
  */
 public interface Qualifiable {
 
-    /** Retourne le score de qualité (entre 0 et 100). */
+    /**
+     * Retourne le score de qualité.
+     *
+     * @return le score de qualité compris entre 0 et 100 inclus
+     */
     int getQualityScore();
 
     /**
-     * Retourne true si le score de qualité est strictement inférieur à 20.
-     * TODO : implémentez cette méthode default en vous appuyant sur
-     * getQualityScore().
+     * Détermine si cet objet est défectueux.
+     *
+     * Un objet est considéré comme défectueux si son score de qualité
+     * est strictement inférieur à 20.
+     *
+     * @return true si le score de qualité est strictement inférieur à 20,
+     *         false sinon
      */
     default boolean isDefective() {
         if (getQualityScore() < 20) {
@@ -26,12 +34,27 @@ public interface Qualifiable {
     }
 
     /**
-     * Retourne un libellé décrivant la qualité :
-     * score >= 80 -> "Excellent" / >= 50 -> "Bon" / >= 20 -> "Médiocre" / < 20 ->
-     * "Défectueux"
-     * TODO : implémentez cette méthode default.
+     * Retourne un libellé décrivant la qualité de cet objet.
+     *
+     * Le libellé est déterminé selon les seuils suivants :
+     * <ul>
+     *   <li>score >= 80 : "Excellent"</li>
+     *   <li>score >= 50 : "Bon"</li>
+     *   <li>score >= 20 : "Médiocre"</li>
+     *   <li>score < 20 : "Défectueux"</li>
+     * </ul>
+     *
+     * @return un libellé de qualité
      */
     default String getQualityLabel() {
-        throw new UnsupportedOperationException("TODO : Qualifiable.getQualityLabel()");
+        if (getQualityScore() >= 80) {
+            return "Excellent";
+        } else if (getQualityScore() >= 50) {
+            return "Bon";
+        } else if (getQualityScore() >= 20) {
+            return "Médiocre";
+        } else {
+            return "Défectueux";
+        }
     }
 }
