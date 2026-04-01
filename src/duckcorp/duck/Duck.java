116a1235688143436ae47469f02f075f856fc037
Bilal ouraghi <ouraghib@3il.fr>
2026-04-01 09:24:33 +0200

    feat : ajout méthode + doc de equals() dans Duck.java

--- src/duckcorp/duck/Duck.java
+++ src/duckcorp/duck/Duck.java
@@ -4,46 +4,70 @@ package duckcorp.duck;
  * Classe abstraite représentant un canard en plastique.
  *
  * TODO (Ex1) :
- *   - Faites implémenter l'interface Qualifiable à cette classe
- *   - Implémentez equals() et hashCode() basés uniquement sur l'id
- *   - Implémentez les méthodes abstraites dans les sous-classes
+ * - Faites implémenter l'interface Qualifiable à cette classe
+ * - Implémentez equals() et hashCode() basés uniquement sur l'id
+ * - Implémentez les méthodes abstraites dans les sous-classes
+ * 
  * @author Roussille Philippe <roussille@3il.fr>
  */
-public abstract class Duck {
+public abstract class Duck implements Qualifiable {
 
     private static int counter = 0;
 
-    private final String   id;
+    private final String id;
     private final DuckType type;
-    private final int      qualityScore;
+    private final int qualityScore;
 
     /** Constructeur fourni. Génère automatiquement un identifiant unique. */
     protected Duck(DuckType type, int qualityScore) {
-        this.id           = type.name().charAt(0) + String.format("%04d", ++counter);
-        this.type         = type;
+        this.id = type.name().charAt(0) + String.format("%04d", ++counter);
+        this.type = type;
         this.qualityScore = Math.max(0, Math.min(100, qualityScore));
     }
 
     // --- Getters fournis ---
 
-    public String   getId()          { return id; }
-    public DuckType getType()        { return type; }
-    public int      getQualityScore(){ return qualityScore; }   // satisfera Qualifiable
+    public String getId() {
+        return id;
+    }
+
+    public DuckType getType() {
+        return type;
+    }
+
+    public int getQualityScore() {
+        return qualityScore;
+    } // satisfera Qualifiable
 
     // --- Méthodes abstraites à implémenter dans les sous-classes ---
 
     public abstract double getBasePrice();
+
     public abstract String describe();
 
     // --- TODO : equals et hashCode ---
 
     /**
-     * Deux canards sont égaux si et seulement si ils ont le même identifiant.
-     * TODO : implémentez equals() en vous basant uniquement sur le champ id.
+     * Compare ce canard avec un autre objet en fonction de son identifiant unique.
+     *
+     * Deux canards sont considérés comme égaux si et seulement si
+     * ils possèdent le même identifiant.
+     *
+     * @param o l'objet à comparer avec ce canard
+     * @return true si les deux objets représentent le même canard
+     *         (même identifiant), false sinon
+     * @see #hashCode()
      */
     @Override
     public boolean equals(Object o) {
-        throw new UnsupportedOperationException("TODO : Duck.equals()");
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Duck duck = (Duck) o;
+        return id.equals(duck.id);
     }
 
     /** TODO : implémentez hashCode() de façon cohérente avec equals(). */
