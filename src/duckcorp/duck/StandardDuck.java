e0f9c01fa6c42fe5c3eada28cb1e113854523897
Bilal ouraghi <ouraghib@3il.fr>
2026-04-01 09:31:57 +0200

    feat: implémentation de la classe StandardDuck.java

--- src/duckcorp/duck/StandardDuck.java
+++ src/duckcorp/duck/StandardDuck.java
@@ -9,7 +9,37 @@ package duckcorp.duck;
  *   - Implémentez getBasePrice() et describe()
  * @author Roussille Philippe <roussille@3il.fr>
  */
-public class StandardDuck {
+public class StandardDuck extends Duck {
 
     public static final double BASE_PRICE = 25.0;
+
+    /**
+     * Crée un nouveau canard standard avec le score de qualité spécifié.
+     *
+     * @param qualityScore le score de qualité du canard (entre 0 et 100)
+     */
+    public StandardDuck(int qualityScore) {
+        super(DuckType.STANDARD, qualityScore);
+    }
+
+    /**
+     * Retourne le prix de base d'un canard standard.
+     *
+     * @return le prix de base : 25.0
+     */
+    @Override
+    public double getBasePrice() {
+        return BASE_PRICE;
+    }
+
+    /**
+     * Retourne une description textuelle de ce canard standard.
+     *
+     * @return la description "Canard Standard"
+     */
+    @Override
+    public String describe() {
+        return "Canard Standard";
+    }
+
 }
